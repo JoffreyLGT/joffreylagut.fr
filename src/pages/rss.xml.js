f1691720f24f601cbfae345a9d35d473252a014c
Joffrey Lagut <j.lagut@live.fr>
2024-01-24 21:33:59 +0100

    fix: rss.xml generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,20 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import createSlug from "src/lib/createSlug";
 
-export async function get(context) {
-  const blog = await getCollection("blog");
+export async function GET(context) {
+  const blog = (await getCollection("blog")) ?? [];
+  const projects = (await getCollection("projects")) ?? [];
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/${post.slug}/`,
+    site: context.site,
+    items: [...blog, ...projects].map((content) => ({
+      title: content.data.title,
+      pubDate: content.data.pubDate,
+      description: content.data.description,
+      link: `/${content.collection}/${createSlug(content.data.title, content.slug)}/`,
     })),
   });
 }
